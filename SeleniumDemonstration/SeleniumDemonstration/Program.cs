cc92793e9ee7d52dd2bb41df74333cd8aef45359
Gangadhar Adusumalli <gangadharadusumalli1993@gmail.com>
2019-02-10 01:13:42 -0600

    initial commit

---
+++ SeleniumDemonstration/SeleniumDemonstration/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+using System.Threading;
+
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace SeleniumDemonstration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Declaring variables for web drivers */
+            IWebDriver driver = new ChromeDriver(System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location));
+            WebDriverWait wait;
+
+          
+                /* Assigning timeout */
+                wait = new WebDriverWait(driver, TimeSpan.FromSeconds(30));
+
+                /* Navigating to google.co.in */
+                driver.Navigate().GoToUrl(@"https://www.google.co.in");
+
+                wait.Until<bool>((d) => { return d.Title.Contains("Google"); });
+                IWebElement searchbox = driver.FindElement(By.Name("q"));
+                searchbox.Click();
+
+                /* Search for "Selenium C#" */
+                searchbox.SendKeys("Selenium C#");
+                IWebElement btnSearch = driver.FindElement(By.Name("btnK"));
+                btnSearch.Submit();
+
+                Thread.Sleep(120000);
+            
+        }
+    }
+}
